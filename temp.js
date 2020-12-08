bcae2e9721efd5cef7332079a5c5049099ab0e21
felax <montminy.felix@gmail.com>
2020-12-08 18:30:24 -0500

    Delete temp.js

--- temp.js
+++
@@ -1,4 +0,0 @@
-* The mouse has left the canvas. Clear out whatever artifacts remain
- * @param {Object} event the mouseout event from the browser.
- * @private
- */Dygraph.prototype.mouseOut_ = function(event){if(this.getFunctionOption("unhighlightCallback")){this.getFunctionOption("unhighlightCallback").call(this,event);}if(this.getBooleanOption("hideOverlayOnMouseOut") && !this.lockedSet_){this.clearSelection();}};
